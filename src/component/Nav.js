7e3ef711f88c81f863569b6cdfe9ac74cdbac926
JC Smiley <jcsmileyjr@gmail.com>
2019-06-06 20:19:30 -0500

    Testing that the Nav is showing on screen

---
+++ src/component/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
+
+import '../App.css';
+
+export default function Nav(props){
+    return(
+        <Container>
+            <Row>
+                <Col>
+                Bob
+                </Col>
+            </Row>
+        </Container>
+    );
+}
