1e8646307d215cd75291d62e02a11710893f0510
JC Smiley <jcsmileyjr@gmail.com>
2019-06-09 09:33:05 -0500

    Refactored Start Game page
    1. Made the starting game view more design responsive
    2. Restructure Nav and Start components to its own view separate from App.js

--- src/App.js
+++ src/App.js
@@ -3,21 +3,42 @@ import React, { Component } from 'react';
 import './App.css';
 import Nav from './component/Nav';
 import Start from './component/Start';
+import { Container, Row, Col } from 'react-bootstrap';
+
+function StartGame(props){
+  return(
+    <Container>
+      <Row>
+        <Col  xs={{span:12}} 
+              sm={{span:10, offset:1}}
+              md={{span:8, offset:2}}
+              lg={{span:6, offset:3}}
+              xl={{span:6, offset:3}}>
+          <Nav money={props.money} />
+          <Start money={props.money} />        
+        </Col>
+      </Row>
+
+    </Container>
+  );
+}
 
 class App extends Component{
   constructor(props){
     super(props);
     this.state={
       funds:100,
+      startPlay: false,
     }
   }
 
   render(){
     return(
-      <div>
-      <Nav money={this.state.funds} />
-      <Start money={this.state.funds} />
-    </div>
+      <Container>
+        {this.state.startPlay === false &&
+        <StartGame money={this.state.funds} />
+        }
+      </Container>
     );
   }
 }
