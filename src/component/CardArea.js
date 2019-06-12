e1657302f83b5fead494e53970204c4ca0c5b640
JC Smiley <jcsmileyjr@gmail.com>
2019-06-11 19:25:26 -0500

    Setup and testing of cards

--- src/component/CardArea.js
+++ src/component/CardArea.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Image} from 'react-bootstrap';
 
 import '../App.css';
 
@@ -7,7 +7,7 @@ function Card(props){
     return(
         <Container className="cardStyle playArea">
             <Row>
-                <Col>{props.cardValue}</Col>
+                <Col><Image src={props.cardValue} rounded fluid /></Col>
             </Row>
         </Container>
     );
@@ -23,8 +23,8 @@ export default function CardArea(props){
                 <Col className="center" xs={2}>10</Col>
             </Row>
             <Row>
-                {props.cardsGiven.map(function(deck){
-                    return <Card cardValue={deck.value} />
+                {props.cardsGiven.map(function(deck, id){
+                    return <Card key={id} cardValue={deck.pic} />
                 })}
             </Row>
         </Container>
--- src/component/cardDeck.js
+++ src/component/cardDeck.js
@@ -1,10 +1,11 @@
+//import aceHeart from '../assets/aceHeart1p.png';
 export const cards = [
-    {"pic":"2h", "value":2, "played":false},
-    {"pic":"3h", "value":3, "played":false},
-    {"pic":"4h", "value":4, "played":false},
-    {"pic":"5h", "value":5, "played":false},
-    {"pic":"6h", "value":6, "played":false},
-    {"pic":"7h", "value":7, "played":false},
+    {"pic":require("../assets/aceHeart1p.png"), "value":2, "played":false},
+    {"pic":require("../assets/aceHeart1p.png"), "value":3, "played":false},
+    {"pic":require("../assets/fiveClubs1p.png"), "value":4, "played":false},
+    {"pic":require("../assets/threeSpades1p.png"), "value":5, "played":false},
+    {"pic":require("../assets/twoSpades1p.png"), "value":6, "played":false},
+    {"pic":require("../assets/aceHeart1p.png"), "value":7, "played":false},
     {"pic":"8h", "value":8, "played":false},
     {"pic":"jh", "value":10, "played":false},
     {"pic":"qh", "value":10, "played":false},
