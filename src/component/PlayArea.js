a205d18c80b66abed713268d7515567e363937c7
JC Smiley <jcsmileyjr@gmail.com>
2019-06-16 22:15:44 -0500

    Refactored PlayArea HTML to use less code

--- src/component/PlayArea.js
+++ src/component/PlayArea.js
@@ -5,6 +5,15 @@ import '../App.css';
 import CardArea from './CardArea';
 import PrimaryButton from './PrimaryButton';
 
+function ButtonChoice(props){
+    return(
+            <Row>
+                <Col xs={4}><PrimaryButton size="smallButtonSize" title={props.title} action={props.action} /></Col>
+                <Col xs={8}><p className="instructionText">{props.text}</p></Col>
+            </Row>
+    );
+}
+
 export default function PlayArea(props){
     return(
         <Container className="appBackgroundColor">
@@ -15,22 +24,14 @@ export default function PlayArea(props){
                 </Col>
             </Row>
             {props.playerTotal < 17 &&
-            <Row>
-                <Col xs={4}><PrimaryButton size="smallButtonSize" title="Hit" action={props.hit} /></Col>
-                <Col xs={8}><p className="instructionText">Get another card. If your new total is over 17, you lose!</p></Col>
-            </Row>            
+            
+                <ButtonChoice title="Hit" action={props.hit} text={"Get another card. If your new total is over 17, you lose!"} />           
             }
 
-            <Row>
-                <Col xs={4}><PrimaryButton size="smallButtonSize" title="Stand" action={props.stand} /></Col>
-                <Col xs={8}><p className="instructionText">Click to end your turn. The dealer starts drawing cards!</p></Col>
-            </Row>
+            <ButtonChoice title="Stand" action={props.stand} text={"Click to end your turn. The dealer starts drawing cards!"} />
 
             {props.playerTotal < 17 &&
-            <Row>
-                <Col xs={4}><PrimaryButton size="smallButtonSize" title="Double Down" action={props.doubleDown} /></Col>
-                <Col xs={8}><p className="instructionText">Double your bet, get one card, and the dealer turn starts!</p></Col>
-            </Row>            
+                <ButtonChoice title={"Double Down"} action={props.doubleDown} text={"Double your bet, get one card, and the dealer turn starts!"} />            
             } 
                                    
         </Container>
